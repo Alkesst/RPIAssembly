ec47c1b7bb489860af3ca8f6c18745c66c26bbfc
Alejandro <alec.augal@gmail.com>
2017-05-02 13:53:09 +0200

    Update calculator.s
    Added the controller of the buttons

--- calculator.s
+++ calculator.s
@@ -1,7 +1,18 @@
-@This code it's supposed to be a calculator in assembly code.
-.data
-
-
+@This code it's supposed to be a calculator in assembly code..
+@For this code you'll need the libberry.s
 .text
 .global main
-main:         
+main:		
+			bl initBerry
+			mov r2, #BUTTON1
+			mov r3, #BUTTON2
+while:		
+			cmp r3, #1
+			beq outwhile
+			cmp r2, #1
+			bneq outwhile		
+			mov r0, #0
+			add r0, r0, #0b1
+			bl setLeds		
+			b while
+outwhile:	
