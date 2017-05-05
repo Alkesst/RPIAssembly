89ed55ac1784565a5270e619eba2de0c3fd00a27
Alejandro <alec.augal@gmail.com>
2017-05-05 20:26:12 +0200

    Update calculator.s

--- calculator.s
+++ calculator.s
@@ -1,32 +1,43 @@
 @This code it's supposed to be a calculator in assembly code..
 @For this code you'll need the libberry.s and the library witingPi.s found at http://wiringpi.com
 .data
-.import "wiringPi.s"
+.include "wiringPiPins.s"
+.include "libberry.s"
 .text
 .global main
-main:		
+main:
+      push {lr}
+      bl opperand
+      mov r4, r0
+      bl opperand
+      mov r5, r0
+      add r0, r4, r5
+      bl setLeds
+      pop {lr}
+      bx lr
 
-endmain:	bx lr
+endmain:
+      bx lr
 
 
-opperand:	
-			push{lr}
+opperand:
+			push {lr}
 			bl initBerry
 			mov r2, #BUTTON1
 			mov r3, #BUTTON2
-while:		
+while:
 			cmp r3, #1
 			beq outwhile
 			cmp r2, #1
-			bneq outwhile		
+			bne outwhile
 			mov r0, #0
 			add r0, r0, #0b1
-			bl setLeds		
+			bl setLeds
 			b while
-outwhile:	
+outwhile:
 			push {r0}
-			mov r0, #
-			mov r1, #
+			ldr r0, =0b100000110
+			ldr r1, =0b1111101000
 			bl playNote
 			pop {r0}
 			pop {lr}
