ab0f93e9624d474ed58eb5eeb29bb7c6968878ae
Alejandro <alec.augal@gmail.com>
2017-05-08 20:19:52 +0200

    Update dicerollSimulator.s

--- dicerollSimulator.s
+++ dicerollSimulator.s
@@ -1,9 +1,10 @@
 @We simulate a dice roll with this code. We'll use WiringPi (found at http://www.wiringpi.com) and libberry.s
 @I'll put some music when you push the botton to give more tension
 .data
-ledssec:    .word   0b000001,0b000011,0b000111,0b001111,0b011111,0b111111
-notassec:   .word   3822, 3034, 2551, 2025, 1607, 1351
-tam:        .word   6
+ledssec:    .word   0b000001,0b000011,0b000111,0b001111,0b011111,0b111111, 0b011111, 0b001111, 0b000111, 0b000011, 0b000001
+            .word
+notassec:   .word   3822, 3034, 2551, 2025, 1911, 1351, 1911, 2025, 2551, 3034, 3822
+tam:        .word   11
 str:        .string "s"
 .include "wiringPiPins.s"
 .text
@@ -23,8 +24,6 @@ random:
 getrand:
         cmp r0, #6
         ble end
-        ldr r0, =str
-        bl puts
         lsl r0, r0, #1
         b getrand
 end:
@@ -39,10 +38,13 @@ while:
         bl digitalRead
         cmp r0, #0
         beq end2
+        mov r0, #1
+        bl delay
         b while
 end2:
         bl random
         mov r5, r0
+        bl soundleds
         bl soundleds
         mov r0, r5
         bl setLeds
@@ -50,23 +52,23 @@ end2:
         bx lr
 
 soundleds:
-        push {r4-r7, lr}
+        push {r4-r8, lr}
         ldr r1, =tam
-        ldr r2, [r1]
+        ldr r8, [r1]
         ldr r5, =ledssec
+        ldr r7, =notassec
 for:
-        cmp r2, #0
+        cmp r8, #0
         beq outfor
         ldr r6, [r5], #4
         mov r0, r6
         bl setLeds
 note:
-        ldr r7, =notassec
         ldr r0, [r7], #4
-        mov r1, #250
+        mov r1, #110
         bl playNote
-        sub r2, r2, #1
+        sub r8, r8, #1
         b for
 outfor:
-        pop {r4-r7, lr}
+        pop {r4-r8, lr}
         bx lr
