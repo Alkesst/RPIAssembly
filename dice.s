839b520927f76a68cbb0fd145f8eb3c280c4a5e9
Alejandro <alec.augal@gmail.com>
2017-05-08 16:42:26 +0200

    Update dice.s

--- dice.s
+++ dice.s
@@ -1,3 +1,5 @@
+@We simulate a dice roll with this code. We'll use WiringPi (found at http://www.wiringpi.com) and libberry.s
+@I'll put some music when you push the botton to give more tension
 .data
 ledssec:    .word   0b000001,0b000011,0b000111,0b001111,0b011111,0b111111
 notassec:   .word   3822, 3034, 2551, 2025, 1607, 1351
