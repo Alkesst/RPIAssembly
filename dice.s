e8721bc59c3e6fd0bff86c2d0ddbe236b1f3c6bb
Alejandro <alec.augal@gmail.com>
2017-05-08 16:37:40 +0200

    Update dice.s

--- dice.s
+++ dice.s
@@ -1,4 +1,7 @@
 .data
+ledssec:    .word   0b000001,0b000011,0b000111,0b001111,0b011111,0b111111
+notassec:   .word   3822, 3034, 2551, 2025, 1607, 1351
+tam:        .word   6
 .include "wiringPi.s"
 .text
 .global main
@@ -7,5 +10,32 @@ main:
 
 
 random:
+        push {lr}
         bl millis
-        
+getrand:
+        cmp r0, #6
+        ble end
+        lsl r0, r0, #1
+        b getrand
+end:
+        pop {lr}
+
+roll:
+        push {lr}
+        mov r4, #BUTTON1
+while:
+        mov r0, r4
+        bl digitalRead
+        cmp r0, #0
+        bne while
+        bl random
+        bl setLeds
+bucl:
+        mov r0, r4
+        bl digitalRead
+        cmp r0, #0
+        beq bucl
+        ldr r0, =7500
+        bl delayMicroseconds
+        bne while
+end2:
