080b26dfd88ca63359d4c466c106781d1bee1a3e
Alejandro <alec.augal@gmail.com>
2017-05-08 10:52:32 +0200

    Update cuadrados.s

--- cuadrados.s
+++ cuadrados.s
@@ -4,8 +4,7 @@ cuad:	@r0 -> n @r4 -> res
         push {r4, lr}	
         cmp r0, #1	@si r0 es igual a 1
         bgt call	@si es mayor salta a call
-        mov r4, r0	@muevo el valor de r0 a r4 
-        b fin		@salto a fin
+        beq fin		@salto a fin
 call:   
 	mul r4, r0, r0  @i^2
         sub r0, r0, #1  @n-1
