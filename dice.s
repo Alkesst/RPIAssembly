0fa4fa932bc5a7067a046e6e92ed145c1ab1d03e
Alejandro <alec.augal@gmail.com>
2017-05-08 17:16:09 +0200

    Update dice.s

--- dice.s
+++ dice.s
@@ -4,10 +4,14 @@
 ledssec:    .word   0b000001,0b000011,0b000111,0b001111,0b011111,0b111111
 notassec:   .word   3822, 3034, 2551, 2025, 1607, 1351
 tam:        .word   6
-.include "wiringPi.s"
+.include "wiringPiPins.s"
 .text
 .global main
 main:
+        push {lr}
+        bl roll
+        pop {lr}
+        bx lr
 
 
 
@@ -24,16 +28,41 @@ end:
         bx lr
 
 roll:
-        push {lr}
+        push {r4, lr}
         mov r4, #BUTTON1
         mov r0, r4
 while:
         bl digitalRead
         cmp r0, #0
         beq end2
+        b while
 end2:
         bl random
         push {r0}
         bl soundleds
         pop {r0}
         bl setLeds
+        pop {r4, lr}
+        bx lr
+
+soundleds:
+        push {r4-r6, lr}
+        ldr r1, =tam
+        ldr r2, [r1]
+        ldr r5, =ledssec
+for:
+        cmp r2, #0
+        beq outfor
+        ldr r6, [r5], #4
+        mov r0, r6
+        bl setLeds
+note:
+        ldr r3, =notassec
+        ldr r0, [r3], #4
+        mov r1, #250
+        bl playNote
+        sub r2, r2, #1
+        b for
+outfor:
+        pop {r4-r6, lr}
+        bx lr
