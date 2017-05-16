6721daab83817186cbf76abbf4fc817514d26ab8
Alejandro <alec.augal@gmail.com>
2017-05-16 16:35:16 +0200

    Update dicerollSimulator.s

--- dicerollSimulator.s
+++ dicerollSimulator.s
@@ -10,72 +10,87 @@ str:        .string "%d\n"
 .text
 .global main
 main:
-        push {lr}
-        bl initBerry
-buc:
-        bl roll
-        ldr r0, =#2500
-        bl delay
-        mov r0, #0
-        bl setLeds
-        mov r0, #BUTTON2
-        bl digitalRead
-        bne buc
-fin:        
-        pop {lr}
-        bx lr
-
+            push {lr}
+            bl initBerry
+cont:
+            bl process
+rpt:
+            mov r0, #BUTTON2
+	bl digitalRead
+	cmp r0, #0
+	beq final
+	mov r0, #BUTTON1
+	bl digitalRead
+	cmp r0, #0
+	beq cont
+	b rpt
+final:
+   
+            pop {lr}
+            bx lr
+            
 
 random:
-        push {lr}
-        bl micros
+            push {lr}
+            bl micros
 getrand:
-        and r2, r0, #0b111
-        sub r0, r2, #1
-        cmp r0, #-1
-        bleq random
-        cmp r0, #0
-        bleq random
-        pop {lr}
-        bx lr
+            and r2, r0, #0b111
+            sub r0, r2, #1
+            cmp r0, #-1
+            bleq random
+            cmp r0, #0
+            bleq random
+            pop {lr}
+            bx lr
 
 roll:
-        push {r4-r5, lr}
+            push {r4-r5, lr}
 while:
-        mov r4, #BUTTON1
-        mov r0, r4
-        bl digitalRead
-        cmp r0, #0
-        beq end2
-        mov r0, #1
-        bl delay
-        b while
+            mov r4, #BUTTON1
+            mov r0, r4
+            bl digitalRead
+            cmp r0, #0
+            beq end2
+            mov r0, #1
+            bl delay
+            b while
 end2:
-        bl soundleds
-        bl soundleds
-        bl random
-        bl setLeds
-        pop {r4-r5, lr}
-        bx lr
+            bl soundleds
+            bl soundleds
+            bl random
+            bl setLeds
+            pop {r4-r5, lr}
+            bx lr
 
 soundleds:
-        push {r4-r8, lr}
-        ldr r1, =tam
-        ldr r8, [r1]
-        ldr r5, =ledssec
-        ldr r7, =notassec
+            push {r4-r8, lr}
+            ldr r1, =tam
+            ldr r8, [r1]
+            ldr r5, =ledssec
+            ldr r7, =notassec
 for:
-        cmp r8, #0
-        beq outfor
-        ldr r6, [r5], #4
-        mov r0, r6
-        bl setLeds
+            cmp r8, #0
+            beq outfor
+            ldr r6, [r5], #4
+            mov r0, r6
+            bl setLeds
 note:
-        ldr r0, [r7], #4
-        mov r1, #110
-        bl playNote
-        sub r8, r8, #1
-        b for
+            ldr r0, [r7], #4
+            mov r1, #110
+            bl playNote
+            sub r8, r8, #1
+            b for
 outfor:
-        pop {r4-r8, lr}
-        bx lr
+            pop {r4-r8, lr}
+            bx lr
+        
+        
+process:
+            push {lr}
+            bl roll
+            ldr r0, =#2500
+            bl delay
+            mov r0, #0
+            bl setLeds
+            pop {lr}
+            bx lr
